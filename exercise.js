76e4c325ede4dbc62608b8101a188b9b537a7f6a
Aiman Al Eryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 03:54:07 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -5,7 +5,10 @@
 // * Convert the `isGraduated` variable to a string data type and print it in the console.
 
 // Try to explain the output of the `hello` variable casting.
-// when trying to convert the String data type to a Number data type, it will always evaluates to NaN (Not a Number), which makes sense, you can't cast strings to numbers, unless the string contains only numbers/floats, in that case it can be casted to a Number data type.
+// when trying to convert the String data type to a Number data type, it will always evaluates to NaN (Not a Number), 
+// which makes sense, you can't cast strings to numbers, unless the string contains only numbers/floats, 
+// in that case it can be casted to a Number data type.
+
 let hello = Number("Ciao"); // number
 let age = Boolean(18); // boolean
 let isGraduated = String(false); // string
